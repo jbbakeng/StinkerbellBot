a2534f5c0bbf6e983474ff7a55bb95007198a8cd
Janne Beate Bakeng <janne.beate.bakeng@gmail.com>
2021-08-23 14:33:21 +0200

    Fixed displaying norwegian characters. There is a limit of 5 items per row and only 5 rows. So now only the 25 first buttons are displayed.

--- stinkerbellbot/sillybuttons.py
+++ stinkerbellbot/sillybuttons.py
@@ -13,17 +13,17 @@ FFMPEG = os.getenv('FFMPEG')
 
 class SillyButton(discord.ui.Button):
 
-    def __init__(self, row: int, label: str, sound_file: str, context: commands.Context):
-        super(SillyButton, self).__init__(label=label, row=row)
+    def __init__(self, row: int, json: str, context: commands.Context):
+        super(SillyButton, self).__init__(label=json['label'], row=row)
         self.context = context
-        self.sound_file = sound_file
+        self.json = json
 
     async def callback(self, interaction: discord.Interaction):
         voice_client = discord.utils.get(self.context.bot.voice_clients, guild=self.context.guild)
         if voice_client.is_connected():
             await interaction.response.defer()
             try:
-                audio_source = discord.FFmpegPCMAudio(executable=FFMPEG, source=f"./sounds/{self.sound_file}")
+                audio_source = discord.FFmpegPCMAudio(executable=FFMPEG, source=f"./sounds/{self.json['file_name']}")
                 voice_client.play(audio_source)
             except discord.errors.ClientException:
                 await self.context.reply(f"Yo {interaction.user.name}, "
@@ -44,11 +44,16 @@ class SillyButtons(discord.ui.View):
         self.sounds_json = "./sounds/sounds.json"
         data = self.read_sounds_json()
 
-        for sound in data['sounds']:
-            self.add_item(SillyButton(0, sound['label'], sound['file_name'], context))
+        row = 0
+        for k, sound in enumerate(data['sounds']):
+            if k % 5 == 4:
+                row+=1
+            if row >= 5:
+                return
+            self.add_item(SillyButton(row, sound, context))
 
     def read_sounds_json(self):
-        f = open(self.sounds_json, 'r')
+        f = open(file=self.sounds_json, mode='r', encoding='utf-8')
         data = json.loads(f.read())
         print(data)
         f.close()
