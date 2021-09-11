7da84d0490cf00e27a5c42e29707638bbc9744d4
Janne Beate Bakeng <janne.beate.bakeng@gmail.com>
2021-09-11 21:21:46 +0200

    #13 Messages are filtered. You get a reply where you send the command, but buttons only show up in default text channel, which is autocreated.

--- stinkerbellbot/bot.py
+++ stinkerbellbot/bot.py
@@ -1,7 +1,7 @@
 """
 bot.py
 ====================================
-The core module
+The core module of the StinkerbellBot
 """
 
 import os
@@ -10,33 +10,60 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
-import stinkerbellbot.sillybuttonsview
+from stinkerbellbot.sillybuttonsview import SillyButtonsView
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 
 class StinkerbellBot(commands.Bot):
-    """The StinkerbellBot itself"""
+    """The StinkerbellBot itself! Bow gracefully!"""
 
     def __init__(self):
         super().__init__(command_prefix='!')
+        self.default_txt_channel = 'command-me'
+        self.default_category = 'StinkerbellBot'
+
+    def get_bot_text_channel(self, guild):
+        return discord.utils.get(guild.channels, name=bot.default_txt_channel)
+
+    def get_bot_channel_category(self, guild):
+        return discord.utils.get(guild.categories, name=bot.default_category)
 
 
 bot = StinkerbellBot()
 
 
+@bot.event
+async def on_ready():
+    """Create a default text channel for itself where it can post messages.
+    Purge the channel and set display help.
+    """
+    for guild in bot.guilds:
+        category = bot.get_bot_channel_category(guild)
+        if category is None:
+            category = await guild.create_category(name=bot.default_category)
+        channel = bot.get_bot_text_channel(guild)
+        if channel is None:
+            channel = await guild.create_text_channel(name=bot.default_txt_channel, category=category)
+
+        deleted = await channel.purge(limit=100)
+
+
 @bot.command(name="sillybuttons")
 async def show_buttons(context: commands.Context):
-    """show_buttons"""
-    views = stinkerbellbot.sillybuttonsview.create_sillybuttonsviews(context)
+    """Create buttons in its sillybuttons channel which will play sounds in the voice channel it has joined"""
+    views = SillyButtonsView.create_sillybuttonsviews(context)
+    channel = bot.get_bot_text_channel(context.guild)
+    if context.channel is not channel:
+        await context.reply(f"Your buttons are ready for you in the #{bot.default_txt_channel}")
     for view in views:
-        await context.send('Press to play a sound', view=view)
+        await channel.send('Press to play a sound', view=view)
 
 
 @bot.command(name="join")
 async def bot_join(context: commands.Context):
-    """bot_join"""
+    """Joins the same voice channel as the user"""
     voice = context.author.voice
     if voice is not None:
         try:
@@ -51,7 +78,7 @@ async def bot_join(context: commands.Context):
 
 @bot.command(name="leave")
 async def bot_leave(context: commands.Context):
-    """leave"""
+    """Leaves the current voice channel"""
     voice_client = discord.utils.get(context.bot.voice_clients, guild=context.guild)
     if voice_client.is_connected():
         await voice_client.disconnect()
